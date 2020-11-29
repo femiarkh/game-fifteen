65134d5e5bfbf07a8c5f88feb962e7670117fa7e
Гачин Александр <gachin.an@yandex.ru>
2020-11-29 11:51:47 +0300

    Clear for review

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import Model from './models/model';
-import View from './views/view';
-import Controller from './controllers/controller';
-
-const gameSize = 4;
-const game = new Controller(new Model(gameSize), new View(gameSize));
-game.init();
