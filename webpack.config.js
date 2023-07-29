bf3de9b479ec9c98e0b206c26a2b23b4a2707271
Dmitrii Gutkovskii <femiarkh@gmail.com>
2023-07-29 17:22:51 +0400

    chore: remove source maps from production

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const path = require('path');
 
+const mode = process.env.NODE_ENV || 'development';
+
 module.exports = {
-  mode: process.env.NODE_ENV || 'development',
+  mode,
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'public'),
   },
-  devtool: 'source-map',
+  devtool: mode === 'development' ? 'source-map' : undefined,
   devServer: {
     static: {
       directory: path.join(__dirname, 'public'),
